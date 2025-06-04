f280acfbef4e15f4825a9dc0f5443025a702bb9c
yinktop <dadaoluyinka79@gmail.com>
2025-06-04 23:52:23 +0100

    Initial commit

--- timestamp.py
+++ timestamp.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
+# timestamp.py
 from datetime import datetime
-
-def print_timestamp():
-    current_time = datetime.now()
-    formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
-    print(f"Current timestamp: {formatted_time}")
-
-if __name__ == "__main__":
-    print_timestamp()
+print(f"Current timestamp: {datetime.now()}")
